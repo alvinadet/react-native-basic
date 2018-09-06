3b1f3b72934dd12f838a5bf42ffa7d684da5b23e
sodojiwo <alvinadetya@rocketmail.com>
2018-09-06 20:41:41 +0700

    form fix

--- screen/Add.js
+++ screen/Add.js
@@ -7,25 +7,58 @@ import {
   Input,
   Label,
   Button,
-  Text
+  Text,
+  Root
 } from 'native-base';
-import { View } from 'react-native';
+
+import { Font, AppLoading } from 'expo';
 
 export default class componentName extends Component {
+  state = {
+    loading: true,
+    huruf: ""
+  };
+
+  componentDidMount() {
+  }
+  ButtonAdd = () => {
+    alert(this.state.huruf);
+    this.setState({
+      huruf: ""
+    })
+  };
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
+    this.setState({ loading: false });
+  }
+
+
   render() {
+    if (this.state.loading) {
+      return (
+        <Root>
+          <AppLoading />
+        </Root>
+      );
+    }
     return (
       <Container>
         <Content>
-          <Form>
-            <Item floatingLabel>
-              <Label>Todos</Label>
-              <Input />
-            </Item>
 
-            <Button>
-              <Text>Click Me!</Text>
-            </Button>
-          </Form>
+          <Item floatingLabel>
+            <Label>Todos</Label>
+            <Input
+              value={this.state.huruf}
+              onChangeText={(text) => this.setState({ huruf: text })} />
+          </Item>
+
+          <Button style={{ marginTop: 10 }} onPress={() => this.ButtonAdd()}>
+            <Text>Tambah</Text>
+          </Button>
+
         </Content>
       </Container>
     );
